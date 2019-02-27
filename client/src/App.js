8c80446d55513a1bda568b9644dd6aa00e2ef9a2
ct <ctaylr13@gmail.com>
2019-02-27 13:37:29 -0500

    rendering front end buttons, state set

--- client/src/App.js
+++ client/src/App.js
@@ -8,10 +8,10 @@ import uuid from 'uuid';
 
 
 class App extends Component {
-  state = {
-    seatRow:
-    [ 
-      [
+  constructor(props) {
+    super(props);
+    this.state = {
+      oldData: [[
           {id:uuid(), seat:"A", row:1, class:"First", occupied:false },
           {id:uuid(), seat:"B", row:1, class:"First", occupied:false },
           {id:uuid(), seat:"D", row:1, class:"First", occupied:true },
@@ -36,14 +36,46 @@ class App extends Component {
           {id:uuid(), seat:"B", row:2, class:"First", occupied:false },
           {id:uuid(), seat:"D", row:2, class:"First", occupied:true },
           {id:uuid(), seat:"E", row:2, class:"First", occupied:false }
-      ]
-    ]
-};
+      ]],
+      seatRow: []
+    };
+  }
+
+  componentDidMount() {
+    var initialArray = [];
+    axios.get(`/api/seats`)
+      .then(res => {
+        initialArray = Object.keys(res.data).map(i => res.data[i]);
+        var finalArray = [];
+        var tempArray = [];
+        for (var i = 0; i < initialArray.length; i++) {  
+          var currentSeat = initialArray[i];
+          var previousSeat = initialArray[i - 1];
+          if( !previousSeat ) {
+              tempArray.push(currentSeat);
+          } else if(previousSeat.row === currentSeat.row) {
+              tempArray.push(currentSeat);
+          } else {
+              finalArray.push(tempArray);
+              tempArray = [currentSeat];
+          }
+        } 
+        finalArray.push(tempArray);
+        this.setState({
+          seatRow: finalArray
+        });
+      })
+  }
+  
   
   render() {
     return (
       <div className="App" >
         <h1>Select a Seat</h1>
+        {this.state.oldData.map((row) => (
+          <SeatRow row={row}/>
+        ))}
+        <h1>Actual data</h1>
         {this.state.seatRow.map((row) => (
           <SeatRow row={row}/>
         ))}
